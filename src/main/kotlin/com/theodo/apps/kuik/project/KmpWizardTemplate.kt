2f29a842df30e9c809c7e50c6e74351cc297de3e
Dennis BORDET <dennisb@bam.tech>
2024-09-09 15:06:30 +0200

    refactor: change image for project generation

--- src/main/kotlin/com/theodo/apps/kuik/project/KmpWizardTemplate.kt
+++ src/main/kotlin/com/theodo/apps/kuik/project/KmpWizardTemplate.kt
@@ -75,7 +75,7 @@ class KmpWizardTemplate {
             )
 
             thumb =
-                { Thumb { URL("https://raw.githubusercontent.com/estivensh4/kotlin-multiplatform-wizard/master/images/project-logo.png") } }
+                { Thumb { URL("https://raw.githubusercontent.com/bamlab/kuist-plugin/master/images/project-logo.png") } }
 
             recipe = { data: TemplateData ->
                 projectRecipe(
