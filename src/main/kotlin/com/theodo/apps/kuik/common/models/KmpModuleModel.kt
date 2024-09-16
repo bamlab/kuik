10e4416378c1c56f9528afa8971e044c1cba925b
Dennis BORDET <dennisb@bam.tech>
2024-09-16 17:05:41 +0200

    fix: platform error

--- src/main/kotlin/com/theodo/apps/kuik/common/models/KmpModuleModel.kt
+++ src/main/kotlin/com/theodo/apps/kuik/common/models/KmpModuleModel.kt
@@ -8,8 +8,8 @@ import com.theodo.apps.kuik.module.model.ModuleType
 
 class KmpModuleModel : WizardModel() {
     var hasAndroid: Boolean by mutableStateOf(true)
-    var hasWeb: Boolean by mutableStateOf(true)
-    var hasIOS: Boolean by mutableStateOf(false)
+    var hasWeb: Boolean by mutableStateOf(false)
+    var hasIOS: Boolean by mutableStateOf(true)
     var hasDesktop: Boolean by mutableStateOf(false)
     var hasServer: Boolean by mutableStateOf(false)
 
