df3bede559db17212bdd2c15391abc5ec4e0cfef
Dennis BORDET <dennisb@bam.tech>
2024-09-24 14:51:40 +0200

    refactor: make NavigationAddOn testable

--- src/main/kotlin/com/theodo/apps/kuik/project/addon/NavigationAddOn.kt
+++ src/main/kotlin/com/theodo/apps/kuik/project/addon/NavigationAddOn.kt
@@ -4,11 +4,13 @@ import com.intellij.ide.fileTemplates.FileTemplateManager
 import com.intellij.ide.starters.local.GeneratorAsset
 import com.intellij.ide.starters.local.GeneratorTemplateFile
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
 import com.theodo.apps.kuik.common.utils.TemplateGroup
 import com.theodo.apps.kuik.common.utils.toFolders
 import com.theodo.apps.kuik.module.KmpModuleRecipe
 import com.theodo.apps.kuik.module.model.ModuleType
 import com.theodo.apps.kuik.project.createEmptyDirectory
+import org.jetbrains.annotations.VisibleForTesting
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
@@ -16,6 +18,7 @@ class NavigationAddOn :
     AddOn(),
     KoinComponent {
     private val kmpModuleRecipe: KmpModuleRecipe by inject()
+    private val ftManager: FileTemplateManager by inject()
 
     override fun getMainProjectFiles(): List<GeneratorAsset> = emptyList()
 
@@ -34,20 +37,26 @@ class NavigationAddOn :
                     model.moduleLowerCase,
                 )
 
-            kmpModuleRecipe.executeRecipe(
-                project = project,
-                model = model,
-                moduleDir = moduleDir,
-                additionalAssets =
-                    listOf(
-                        GeneratorTemplateFile(
-                            "src/commonMain/kotlin/${model.packageName.toFolders()}/${model.moduleLowerCase}/MainDestination.kt",
-                            FileTemplateManager.getDefaultInstance().getCodeTemplate(TemplateGroup.NAVIGATION_DESTINATIONS),
-                        ),
-                    ),
-            )
+            generateAssets(project, moduleDir)
         } else {
             emptyList()
         }
     }
+
+    @VisibleForTesting
+    fun generateAssets(
+        project: Project,
+        moduleDir: VirtualFile,
+    ) = kmpModuleRecipe.executeRecipe(
+        project = project,
+        model = model,
+        moduleDir = moduleDir,
+        additionalAssets =
+            listOf(
+                GeneratorTemplateFile(
+                    "src/commonMain/kotlin/${model.packageName.toFolders()}/${model.moduleLowerCase}/MainDestination.kt",
+                    ftManager.getCodeTemplate(TemplateGroup.NAVIGATION_DESTINATIONS),
+                ),
+        ),
+    )
 }
---
+++ src/test/kotlin/com/theodo/apps/kuik/project/addon/NavigationAddOnTest.kt
@@ -0,0 +1,77 @@
+package com.theodo.apps.kuik.project.addon
+
+import com.intellij.ide.fileTemplates.FileTemplateManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import com.theodo.apps.kuik.assertListEquals
+import com.theodo.apps.kuik.module.KmpModuleRecipe
+import com.theodo.apps.kuik.module.ModuleAssetGenerator
+import com.theodo.apps.kuik.module.extrafilemodifier.AddKoinModuleToMainKoinModule
+import com.theodo.apps.kuik.module.extrafilemodifier.AddModuleDepsToMainApp
+import com.theodo.apps.kuik.module.extrafilemodifier.AddModuleToSettingsGradle
+import com.theodo.apps.kuik.module.extrafilemodifier.AddScreenRoute
+import com.theodo.apps.kuik.module.extrafilemodifier.AddScreenToNavHost
+import com.theodo.apps.kuik.project.ProjectAssetGenerator
+import io.mockk.mockkClass
+import org.junit.Rule
+import org.junit.Test
+import org.koin.core.module.dsl.factoryOf
+import org.koin.dsl.module
+import org.koin.test.KoinTest
+import org.koin.test.KoinTestRule
+import org.koin.test.get
+import org.koin.test.inject
+import org.koin.test.mock.MockProviderRule
+import org.koin.test.mock.declareMock
+
+class NavigationAddOnTest : KoinTest {
+    @get:Rule
+    val koinTestRule =
+        KoinTestRule.create {
+            modules(
+                module {
+                    declareMock<AddKoinModuleToMainKoinModule>()
+                    declareMock<AddModuleDepsToMainApp>()
+                    declareMock<AddModuleToSettingsGradle>()
+                    declareMock<AddScreenRoute>()
+                    declareMock<AddScreenToNavHost>()
+                    declareMock<FileTemplateManager>()
+                    declareMock<ProjectAssetGenerator>()
+                    declareMock<ProjectAssetGenerator>()
+                    declareMock<Project>()
+                    declareMock<VirtualFile>()
+                    declareMock<ModuleAssetGenerator>()
+                    factoryOf(::KmpModuleRecipe)
+                    factoryOf(::NavigationAddOn)
+                },
+            )
+        }
+
+    @get:Rule
+    val mockProvider = MockProviderRule.create { clazz -> mockkClass(type = clazz, relaxed = true) }
+
+    @Test
+    fun `WHEN add on generate assets then it return the correct assets`() {
+        // Given
+        val addOn by inject<NavigationAddOn>()
+        addOn.initialize("com.theodo.apps.kuik")
+
+        // When
+        val assets = addOn.generateAssets(get(), get())
+
+        // Then
+
+        assertListEquals(
+            expected =
+                listOf(
+                    "src/commonMain/kotlin/com/theodo/apps/kuik/core/navigation/MainDestination.kt",
+                    "src/commonMain/kotlin/com/theodo/apps/kuik/core/navigation",
+                    "src/commonMain/kotlin/com/theodo/apps/kuik/core/navigation/di/navigationKoinModule.kt",
+                    "src/androidMain/kotlin/com/theodo/apps/kuik/core/navigation",
+                    "src/iosMain/kotlin/com/theodo/apps/kuik/core/navigation",
+                    "build.gradle.kts",
+                ),
+            actual = assets.map { it.relativePath },
+        )
+    }
+}
