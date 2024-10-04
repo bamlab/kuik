4fd9da9843926862534527ae67df611bc4f15936
Dennis BORDET <dennisb@bam.tech>
2024-10-04 16:53:06 +0200

    feat: update versions

--- src/test/kotlin/com/theodo/apps/kuik/module/model/mockContent/MainModuleBuild.kt
+++ src/test/kotlin/com/theodo/apps/kuik/module/model/mockContent/MainModuleBuild.kt
@@ -16,7 +16,7 @@ kotlin {
     androidTarget {
         @OptIn(ExperimentalKotlinGradlePluginApi::class)
         compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
+            jvmTarget.set(JvmTarget.JVM_21)
         }
     }
 
@@ -86,8 +86,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     buildFeatures {
         compose = true
